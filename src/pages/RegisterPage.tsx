84dcc205816964884399bdaf73cf8d0fe6b73692
Kirill <kirezamba34@mail.ru>
2022-03-13 14:40:01 +0300

    refactor

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,16 +1,7 @@
-import React from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import SignUp from "../components/SignUp";
-import { useAuth } from "../hooks/useAuth";
 
 export default function RegisterPage() {
-  const { isAuth } = useAuth();
-  const navigate = useNavigate();
-  React.useEffect(() => {
-    if (isAuth) {
-      return navigate("/login");
-    }
-  }, [isAuth, navigate]);
   return (
     <div className="container-md">
       <h1>Register</h1>
